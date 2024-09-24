4a388c500555a5b4c05dcb7e2acf2249b95fc8af
Alexander <alexander.demidov.1997@gmail.com>
2024-09-24 03:37:18 +0300

    add typeorm and config to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScaleModule } from './scale/scale.module';
 
 @Module({
-  imports: [ScaleModule],
+  imports: [ConfigModule.forRoot({envFilePath: ".env"}),
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => {
+        await ConfigModule.envVariablesLoaded;
+
+        return {
+          type: "postgres",
+          host: process.env.DATABASE_HOST,
+          port: +process.env.DATABASE_PORT,
+          username: process.env.DATABASE_USERNAME,
+          password: process.env.DATABASE_PASSWORD,
+          database: process.env.DATABASE_NAME,
+          entities: [],
+          synchronize: false
+        }
+      }
+    }), ScaleModule],
   controllers: [AppController],
   providers: [AppService],
 })
